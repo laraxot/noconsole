3b61586d815aa29f7ab25a8089af6e5871e1a2e2
marco76tv <marco.sottana@gmail.com>
2019-03-27 13:08:59 +0100

    .

--- password.php
+++ password.php
@@ -1,8 +1,16 @@
 <?php
+<<<<<<< HEAD
 
 $password = 'password1234';
 if (!isset($_SERVER['PHP_AUTH_USER']) || $_SERVER['PHP_AUTH_PW'] !== $password) {
     \header('WWW-Authenticate: Basic realm="NoConsoleComposer"');
     \header('HTTP/1.0 401 Unauthorized');
+=======
+$password = "password1234";
+if (!isset($_SERVER['PHP_AUTH_USER']) || $_SERVER['PHP_AUTH_PW'] !== $password)
+{
+    header('WWW-Authenticate: Basic realm="NoConsoleComposer"');
+    header('HTTP/1.0 401 Unauthorized');
+>>>>>>> 33a6daa (.)
     exit;
 }
