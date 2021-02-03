f12f186e92657560269daa8ba1147778780aec68
marco76tv <marco.sottana@gmail.com>
2021-02-03 11:29:42 +0100

    .

--- index.php
+++ index.php
@@ -525,9 +525,16 @@ $artisan_cmds = ['route:list', 'view:clear'];
 <head>
     <title>NoConsole</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<<<<<<< HEAD
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
     <script type="text/javascript" src="//code.jquery.com/jquery-2.1.1.min.js"></script>
     <script type="text/javascript" src="js/script.js"></script>
+=======
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" />
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"
+        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="js/script.js<?php echo '?rnd='.rand(1, 100); ?>"></script>
+>>>>>>> b10f238 (.)
     <link href="css/style.css" media="all" rel="stylesheet" />
 </head>
 
