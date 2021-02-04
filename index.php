e50313f7dc5a8f2d75b214f88a6406ff44d7f0b8
marco76tv <marco.sottana@gmail.com>
2021-02-04 23:21:18 +0100

    .

--- index.php
+++ index.php
@@ -527,7 +527,11 @@ $cmds = [
         //'getStatus',//is a function not a command
     ],
     'Bower' => ['install', 'remove', 'update', 'list'],
-    'Artisan' => ['route:list', 'view:clear', 'migrate', 'route:cache', 'vendor:publish','db:seed'],
+    'Artisan' => ['route:list',
+        'route:clear', 'view:clear', 'optimize:clear', 'cache:clear',
+        'config:cache', 'config:clear',
+        'migrate', 'route:cache', 'vendor:publish', 'db:seed',
+    ],
 ];
 >>>>>>> e03e7a1 (.)
 ?>
@@ -546,10 +550,15 @@ $cmds = [
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"
         integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <<<<<<< HEAD
+<<<<<<< HEAD
     <script type="text/javascript" src="js/script.js<?php echo '?rnd='.rand(1, 100); ?>"></script>
 >>>>>>> b10f238 (.)
 =======
     <script type="text/javascript" src="js/script.js<?php echo '?rnd='.rand(1, 100); ?>">
+=======
+    <script type="text/javascript"
+        src="js/script.js<?php echo '?rnd='.rand(1, 100); ?>">
+>>>>>>> f681c50 (.)
     </script>
 >>>>>>> e03e7a1 (.)
     <link href="css/style.css" media="all" rel="stylesheet" />
