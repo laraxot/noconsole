d7577a9607d80915900df0512b8669bc0e926612
Malebestia <vair81@gmail.com>
2024-01-10 11:08:09 +0100

    add filament:upgrade

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
  */
 declare(strict_types=1);
 error_reporting(E_ALL);
-ini_set('display_errors','true');
+ini_set('display_errors', 'true');
 require_once 'DotEnv.php';
-(new DotEnv(__DIR__.'/.env'))->load();
+(new DotEnv(__DIR__ . '/.env'))->load();
 
 
 include 'password.php';
@@ -16,7 +16,7 @@ include 'password.php';
 //die('['.__LINE__.']['.getenv('LARAVEL_DIR').']['.$_ENV['LARAVEL_DIR'].']');
 
 //$base_path = realpath('../../laravel');
-$base_path=realpath(__DIR__.'/'.getenv('LARAVEL_DIR'));
+$base_path = realpath(__DIR__ . '/' . getenv('LARAVEL_DIR'));
 
 /*
 die(print_r([
@@ -44,6 +44,9 @@ $cmds = [
         'migrate',
         'route:cache', 'vendor:publish',
     ],
+    'Filament' => [
+        'filament:upgrade'
+    ],
     'Env' => [
         'debug:on', 'debug:off',
     ],
@@ -61,7 +64,7 @@ $cmds = [
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" />
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"
         integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="js/script.js<?php echo '?rnd='.rand(1, 100); ?>">
+    <script type="text/javascript" src="js/script.js<?php echo '?rnd=' . rand(1, 100); ?>">
     </script>
     <link href="css/style.css" media="all" rel="stylesheet" />
 </head>
@@ -78,17 +81,17 @@ $cmds = [
                 <button onclick="extractComposer()">extractComposer</button>
                 <?php
                     $html = '';
-                    foreach ($cmds as $pack => $pack_cmds) {
-                        $html .= '<fieldset>
-                    <legend>'.$pack.' Commands</legend>
-                    <input type="text" class="form-input" name="package" id="'.$pack.'_text" style="width:100%">&nbsp;<br/>';
-                        foreach ($pack_cmds as $cmd) {
-                            $html .= '<button onclick="callPack(\''.$cmd.'\',\''.$pack.'\')" class="btn btn-success">'.$cmd.'</button>&nbsp;';
-                        }
-                        $html .= '</fieldset>';
-                    }
-                    echo $html;
-                ?>
+foreach ($cmds as $pack => $pack_cmds) {
+    $html .= '<fieldset>
+                    <legend>' . $pack . ' Commands</legend>
+                    <input type="text" class="form-input" name="package" id="' . $pack . '_text" style="width:100%">&nbsp;<br/>';
+    foreach ($pack_cmds as $cmd) {
+        $html .= '<button onclick="callPack(\'' . $cmd . '\',\'' . $pack . '\')" class="btn btn-success">' . $cmd . '</button>&nbsp;';
+    }
+    $html .= '</fieldset>';
+}
+echo $html;
+?>
 
             </div>
             <h3>Console Output:</h3>
