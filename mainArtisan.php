95cd4677e2ae124bdc8dc480b97a132f788ea348
marco76tv <marco.sottana@gmail.com>
2024-01-10 11:24:50 +0100

    .

--- mainArtisan.php
+++ mainArtisan.php
@@ -22,32 +22,33 @@ if (! \function_exists($_POST['function'])) {
 */
 include 'common.php';
 
-function command() {
+function command()
+{
     $root_dir = '../../laravel';
     if (! file_exists(ROOT_DIR.'/vendor/autoload.php')) {
         $root_dir = '../..';
         if (! file_exists(ROOT_DIR.'/vendor/autoload.php')) {
-            die(print_r([
-                'error '=>'file not exists',
-                'file'=>ROOT_DIR.'/vendor/autoload.php',
-                'line'=> __LINE__,
-                'file'=>__FILE__,
-            ],true));
+            exit(print_r([
+                'error ' => 'file not exists',
+                'file' => ROOT_DIR.'/vendor/autoload.php',
+                'line' => __LINE__,
+                'file' => __FILE__,
+            ], true));
         }
     }
     require_once ROOT_DIR.'/vendor/autoload.php';
     $app = require_once ROOT_DIR.'/bootstrap/app.php';
     $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
 
-    //$vars = [$_POST['command']];
-    //$input = new Symfony\Component\Console\Input\ArrayInput($vars);
+    // $vars = [$_POST['command']];
+    // $input = new Symfony\Component\Console\Input\ArrayInput($vars);
 
     $command = $_POST['command'];
     if (isset($_POST['package']) && \mb_strlen(\trim($_POST['package'])) > 3 && 'exe' == $command) {
         $command = $_POST['package'];
     }
     $input = new Symfony\Component\Console\Input\StringInput($command);
-    //$output = new Symfony\Component\Console\Output\ConsoleOutput();
+    // $output = new Symfony\Component\Console\Output\ConsoleOutput();
     $output = new Symfony\Component\Console\Output\StreamOutput(\tmpfile());
     $status = $kernel->handle($input, $output);
 
